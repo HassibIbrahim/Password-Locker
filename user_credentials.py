c76e2bbd1d2777b6d5b6b97d1cd61c4d6ae4051d
Hassib Ibrahim <ihassib@gmail.comclearcd cleargit -lgit --lostgit config --global user.email ihassib@gmail.com>
2018-11-02 12:55:09 +0300

    reran a test for user object input

--- user_credentials.py
+++ user_credentials.py
@@ -5,13 +5,14 @@ class User:
     """
     Class that generates new instances of user
     """
+    user_list=[]
 
     def __init__(self, first_name, last_name, email, password):
         '''
         __init__ method that define properties held in our objects.
         '''
 
-        self.first_name: first_name
-        self.last_name: last_name
-        self.email: email
-        self.password: password
+        self.first_name= first_name
+        self.last_name= last_name
+        self.email= email
+        self.password= password
--- user_credentials_test.py
+++ user_credentials_test.py
@@ -29,4 +29,4 @@ class TestUserCredentials(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
