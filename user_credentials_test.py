d3796fb88fda436d9a17f54b5c66b48430e5ca81
Hassib Ibrahim <ihassib@gmail.comclearcd cleargit -lgit --lostgit config --global user.email ihassib@gmail.com>
2018-11-02 12:21:06 +0300

    Created a new class User

---
+++ user_credentials_test.py
@@ -0,0 +1,16 @@
+import unittest #importing the unit test
+from user_credentials import User 
+
+
+class TestUserCredentials(unittest.TestCase):
+    '''
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestCase :TestCase class that helps in creating test cases 
+    '''
+
+ 
+
+if __name__ == '__main__':
+    unittest.main()
