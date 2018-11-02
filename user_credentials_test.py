bb318bc88cd963bfac1e8038f0fbc1f727798b16
Hassib Ibrahim <ihassib@gmail.comclearcd cleargit -lgit --lostgit config --global user.email ihassib@gmail.com>
2018-11-02 12:27:02 +0300

    ran a test on save user content

--- user_credentials_test.py
+++ user_credentials_test.py
@@ -1,4 +1,5 @@
 import unittest #importing the unit test
+import pyperclip
 from user_credentials import User 
 
 
@@ -10,7 +11,22 @@ class TestUserCredentials(unittest.TestCase):
         unittest.TestCase :TestCase class that helps in creating test cases 
     '''
 
- 
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.  
+        '''
+        self.new_user= User("Ibrahim","Hassib","ihassib@gmail.com","McGee")
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.first_name,"Ibrahim")
+        self.assertEqual(self.new_user.last_name,"Hassib")
+        self.assertEqual(self.new_user.email,"ihassib@gmail.com")
+        self.assertEqual(self.new_user.password,"McGee")
+
 
 if __name__ == '__main__':
     unittest.main()
