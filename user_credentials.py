ac8b87d385548e824639352e256a9ce33dc1fbb1
Hassib Ibrahim <ihassib@gmail.comclearcd cleargit -lgit --lostgit config --global user.email ihassib@gmail.com>
2018-11-04 12:06:58 +0300

    ran a test to save account information...passed

--- user_credentials.py
+++ user_credentials.py
@@ -1,22 +1,21 @@
 import pyperclip
 
 
-
 class User:
     """
     Class that generates new instances of user
     """
-    user_list=[]
+    user_list = []
 
     def __init__(self, first_name, last_name, email, password):
         '''
         __init__ method that define properties held in our objects.
         '''
 
-        self.first_name= first_name
-        self.last_name= last_name
-        self.email= email
-        self.password= password
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
 
     def save_user(self):
         '''
@@ -24,9 +23,10 @@ class User:
         '''
         User.user_list.append(self)
 
+
 class Credential:
     '''
-    Class that creates instances of user credentials 
+    Class that creates instances of user credentials
     '''
     credential_list = []
 
@@ -37,5 +37,9 @@ class Credential:
         self.site_name = site_name
         self.account_name = account_name
         self.password = password
-        
-  
+
+    def save_credentials(self):
+            '''
+	        Function to save a newly created user instance
+	        '''
+            Credential.credential_list.append(self)
--- user_credentials_test.py
+++ user_credentials_test.py
@@ -1,7 +1,9 @@
-import unittest #importing the unit test
+import unittest  # importing the unit test
 import pyperclip
+
 from user_credentials import User
 from user_credentials import Credential
+
 
 
 class TestUser(unittest.TestCase):
@@ -16,17 +18,17 @@ class TestUser(unittest.TestCase):
         '''
         Set up method to run before each test cases.  
         '''
-        self.new_user= User("Ibrahim","Hassib","ihassib@gmail.com","McGee")
+        self.new_user = User("Ibrahim", "Hassib", "ihassib@gmail.com", "McGee")
 
     def test_init(self):
         '''
         test_init test case to test if the object is initialized properly
         '''
 
-        self.assertEqual(self.new_user.first_name,"Ibrahim")
-        self.assertEqual(self.new_user.last_name,"Hassib")
-        self.assertEqual(self.new_user.email,"ihassib@gmail.com")
-        self.assertEqual(self.new_user.password,"McGee")
+        self.assertEqual(self.new_user.first_name, "Ibrahim")
+        self.assertEqual(self.new_user.last_name, "Hassib")
+        self.assertEqual(self.new_user.email, "ihassib@gmail.com")
+        self.assertEqual(self.new_user.password, "McGee")
 
     def test_save_user(self):
         '''
@@ -35,7 +37,8 @@ class TestUser(unittest.TestCase):
         '''
 
         self.new_user.save_user()
-        self.assertEqual(len(User.user_list),1)
+        self.assertEqual(len(User.user_list), 1)
+
 
 class TestCredentials(unittest.TestCase):
     '''
@@ -44,30 +47,30 @@ class TestCredentials(unittest.TestCase):
     Args:
         unittest.TestCase :TestCase class that helps in creating test cases 
     '''
+
     def setUp(self):
         '''
         Set up method to run before each test cases.  
         '''
-        self.new_account = Credential("Twitter","Hiasco","McGee")
+        self.new_account = Credential("Twitter", "Hiasco", "McGee")
 
     def test_init_(self):
         '''
         test_init test case to test if the object is initialized properly
         '''
-        self.assertEqual(self.new_account.site_name,'Twitter')
-        self.assertEqual(self.new_account.account_name,'Hiasco')
-        self.assertEqual(self.new_account.password,'McGee') 
+        self.assertEqual(self.new_account.site_name, 'Twitter')
+        self.assertEqual(self.new_account.account_name, 'Hiasco')
+        self.assertEqual(self.new_account.password, 'McGee')
 
     def test_save_credentials(self):
         '''
         test_save_credentials to see if the new account information is saved into the appropriate list
         '''
         self.new_account.save_credentials()
-        self.facebook = Credential('Facebook','Loso','psd001')
+        self.facebook = Credential('Facebook', 'Loso', 'psd001')
         self.facebook.save_credentials()
-        self.assertEqual(len(Credential.credential_list),2)
+        self.assertEqual(len(Credential.credential_list), 2)
 
-    
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
